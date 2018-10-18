d9f8a17f7d9f9342f6df59d4007b3ea59af4b586
yigangxi <964496925@qq.com>
2018-10-18 13:54:42 +0800

    初识进程间通信之管道

--- learnunix/test.c
+++ learnunix/test.c
@@ -1,47 +1,51 @@
-#include "apue.h"
-#include <setjmp.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <error.h>
+#include <string.h>
 
-static void f1(int,	int, int, int);
-static void f2(void);
-
-static	jmp_buf	jmpbuffer;
-static	int		globval;
-
-int	main(void){
-	int	autoval;
-	register	int	regival;
-	volatile	int volaval;
-	static		int	statval;
-
-	globval	= 1;
-	autoval = 2;
-	regival = 3;
-	volaval = 4;
-	statval = 5;
-
-	if(setjmp(jmpbuffer) != 0){
-		printf("after longjmp:\n");
-		printf("globval = %d, autoval = %d, regival = %d,"
-		" volaval = %d, statval = %d\n",globval,autoval,regival,volaval,statval);
-		exit(0);
+int main(){
+	int _pipe[2];
+	int ret = pipe(_pipe);//类似两个文件指针，针对管道中的读(0)和写(1)
+	if(ret == -1){
+		printf("创建管道失败，错误码是:%d,%d\n",error,strerror(error));
+		return 1;
 	}
 
-	globval	= 91;
-	autoval = 92;
-	regival = 93;
-	volaval = 94;
-	statval = 95;
+	pid_t pid = fork();//创建子进程
 
-	f1(autoval,regival,volaval,statval);
-	//exit(0);
-}
+	if(pid < 0){
+		printf("创建子进程错误\n");
+		return 2;
+	}
 
-static void f1(int i, int j, int k, int l){
-	printf("in f1():\n");
-	printf("globval = %d, autoval = %d, regival = %d,"
-	" volaval = %d, statval = %d\n",globval,i,j,k,l);
-	f2();
-}
-static void f2(void){
-	longjmp(jmpbuffer,1);
+	else if(pid == 0){  //子进程的操作
+		close(_pipe[0]);  // 关闭读通道
+		int i = 0;
+		char mesg[1024];
+		while( i < 20){
+			scanf("%s", mesg);
+			write(_pipe[1], mesg, strlen(mesg));
+			sleep(2);
+			i++;
+		}
+	}
+	else{    //进入父进程，从子进程读取数据
+		close(_pipe[1]);  //关闭写通道
+		char _mesg[1024];
+		int j = 0;
+		while(j < 20){
+			memset(_mesg, 0, sizeof(_mesg));
+			int ret = read(_pipe[0], _mesg, sizeof(_mesg));
+			if(strstr(_mesg, "quit") != 0){
+				printf("退出程序\n");
+				exit(0);
+			}
+			printf("message:%s. length:%d. count:%d\n", _mesg, ret, ++j);
+		}
+
+		if(waitpid(pid, NULL, 0) < 0){  //避免父进程与子进程冲突，让子进程先执行
+			return 3;
+		}
+	}
+	return 0;
 }
